b11b5934d97093473a82254edab45e8bbf30dea5
bhupendra singh <yuvibana03@gmail.com>
2023-10-17 02:01:41 +0530

    home added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './App.css'
 import { Routes, Route, Outlet } from 'react-router-dom'
-// import Home from './components/Home'
+import Home from './components/Home'
 
 function App() {
 
@@ -9,9 +9,9 @@ function App() {
     <>
 
       <Outlet />
-      {/* <Routes>
+      <Routes>
         <Route path='/' element={<Home />} />
-      </Routes> */}
+      </Routes>
     </>
   )
 }
